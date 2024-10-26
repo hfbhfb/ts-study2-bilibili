acbbc08b29bae46da5b40b0a494a6c9eeebd7096
hfbhfb <hefabao@126.com>
2024-10-26 12:58:11 +0800

    修改tsconfig后，全局修改String prototyped在ts中使用是生效的

---
+++ src/module/c.js
@@ -0,0 +1,8 @@
+// 注：这里ts引用，和ts里的string有什么差别
+String.prototype.getFirstLetter = function () {
+  return 'sfss111'
+}
+
+console.log('aa111')
+const llstr = 'dsf'
+console.log(llstr.getFirstLetter())
--- src/module/index.ts
+++ src/module/index.ts
@@ -1,8 +1,14 @@
 import './a'
-import { name as nameProp} from './b'
+import './c'
+import { name as nameProp } from './b'
+
 import * as info from './b'
-console.log('nameProp', nameProp);
-console.log('info', info);
-console.log("hello1");
-console.log("hello1");
-console.log("hello1");
+console.log('nameProp', nameProp)
+console.log('info', info)
+console.log('hello1')
+const aa: string = 'aa111'
+
+console.log(aa.getFirstLetter())
+console.log(aa.getFirstLetter())
+console.log(aa.getFirstLetter())
+console.log(aa.getFirstLetter())
