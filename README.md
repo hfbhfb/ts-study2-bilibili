752bd44bf92e6f7c7ee3c7e57aaf73dfd8d89b96
hfbhfb <hefabao@126.com>
2024-10-26 11:23:21 +0800

    1

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+# ts视频教程
+https://github.com/zhangjianguo123/TS
+
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// import './example/basic-type' // 演绎基础类型
+// import './example/symbol' // symbol类型
+// import './example/interface' // 接口类型
+// import './example/function' // 函数
+// import './example/generics' // 泛型
+// import './example/class' // 类
+// import './example/enum' // 枚举
+// import './example/interence-compatibility' // 类型推断和兼容性
+// import './example/advanced-type-1' // 高级类型1
+// import './example/advanced-type-2' // 高级类型2
+import './module/index'
+document.title = 'test_ts'
